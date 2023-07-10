6271c4e4b0a4a35dcfd04450d8d90e2001239f8d
Brian Segura <briansegura093@gmail.com>
2023-07-10 14:36:30 -0400

    up to page 474 of da BOOK en colombia pa surmese

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,20 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
   def create
-    render "new"
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Log the user in and redirect to the user's show page.
+      reset_session
+      log_in user
+      redirect_to_ user
+    else
+      # Create an error message.
+      flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
+      render 'new'
+    end  
   end
 
   def destroy
