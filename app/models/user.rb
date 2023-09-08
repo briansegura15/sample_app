b0412e3984a94ad4c6358db79ed27a411d3b53a7
Brian Segura <briansegura093@gmail.com>
2023-09-08 10:16:43 -0400

    finito chapter 12, vamos por la 13

--- app/models/user.rb
+++ app/models/user.rb
@@ -64,9 +64,10 @@ class User < ApplicationRecord
     # Sets the password reset attributes.
     def create_reset_digest
         self.reset_token = User.new_token
-        update_attribute(:reset_digest, User.digest(reset_token))
-        update_attribute(:reset_sent_at, Time.zone.now)
-    end
+        self.reset_digest = User.digest(reset_token)
+        self.reset_sent_at = Time.zone.now
+        update_columns(reset_digest: reset_digest, reset_sent_at: reset_sent_at)
+      end
 
     # Sends password reset email.
     def send_password_reset_email
