9e9c5bd13f7cc8ea357f2253940e559120e48b12
Brian Segura <briansegura093@gmail.com>
2023-09-18 12:59:47 -0400

    last section for chapter 13, then its 14, then its 100hours, then its gametime. On page 811

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -11,7 +11,7 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
 
   test "should redirect create when not logged in" do
     assert_no_difference 'Micropost.count' do
-    post microposts_path, params: { micropost: { content: "Lorem ipsum" } }
+      post microposts_path, params: { micropost: { content: "Lorem ipsum" } }
     end
     assert_redirected_to login_url
   end
@@ -23,5 +23,13 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
     
+  test "should redirect destroy for wrong micropost" do
+    log_in_as(users(:michael))
+    micropost = microposts(:ants)
+    assert_no_difference 'Micropost.count' do
+      delete micropost_path(micropost)
+    end
+    assert_redirected_to root_url
+  end
     
 end
